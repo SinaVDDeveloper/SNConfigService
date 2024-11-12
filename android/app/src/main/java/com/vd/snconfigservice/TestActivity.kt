e92ed86471582f4c4b0b995ecd29e63290b1da0a
sunxiao5 <sunxiao5@staff.sina.com.cn>
2024-11-12 11:30:08 +0800

    android init version
    android init version

---
+++ android/app/src/main/java/com/vd/snconfigservice/TestActivity.kt
@@ -0,0 +1,12 @@
+package com.vd.snconfigservice
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import snconfigservice.R
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_test_activity)
+    }
+}
